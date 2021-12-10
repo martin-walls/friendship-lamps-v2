2528166ec96f4416f4eba147a7340c0d60c7e66f
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2021-12-10 11:43:46 +0000

    check for error after compile

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
+@echo off
+
 set port=COM3
 set board=esp32:esp32:pico32
 set sketch=friendship-lamps-v2.ino
 
 arduino-cli compile --fqbn %board% %sketch%
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
 arduino-cli upload -p %port% --fqbn %board% %sketch%
