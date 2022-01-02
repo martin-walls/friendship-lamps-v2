c8b7a4af70dd87118b61699bf3fd824d09601175
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-01-02 14:47:34 +0000

    combine conf into one file

--- conf.h
+++ conf.h
@@ -6,41 +6,27 @@
 #define THING_INDEX THING_S
 
 #if THING_INDEX == 0
-#define VPIN_COLOR_SEND V0
-#define VPIN_COLOR_READ V1
-
 #define VPIN_STATUS_SEND V2
 #define VPIN_STATUS_READ V3
-
-#define VPIN_EFFECT_SEND V4
-#define VPIN_EFFECT_READ V5
-
-#define VPIN_ZERGBA_READ V6
-#define VPIN_APP_COLOR_LED V7
 
 #define VPIN_SEND V8
 #define VPIN_READ V9
 
-#include "secrets/secrets_0.h"
-
-#else
-#define VPIN_COLOR_SEND V1
-#define VPIN_COLOR_READ V0
-
-#define VPIN_STATUS_SEND V3
-#define VPIN_STATUS_READ V2
-
-#define VPIN_EFFECT_SEND V5
-#define VPIN_EFFECT_READ V4
-
 #define VPIN_ZERGBA_READ V6
 #define VPIN_APP_COLOR_LED V7
+
+#else
+#define VPIN_STATUS_SEND V3
+#define VPIN_STATUS_READ V2
 
 #define VPIN_SEND V9
 #define VPIN_READ V8
 
-#include "secrets/secrets_1.h"
+#define VPIN_ZERGBA_READ V6
+#define VPIN_APP_COLOR_LED V7
 
 #endif
+
+#include "secrets/secrets.h"
 
 #endif
